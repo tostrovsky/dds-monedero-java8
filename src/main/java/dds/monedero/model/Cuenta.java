daee2cd0e7a27bceef07c87a823949a5f4df01b6
Tomas Ostrovsky <totostrovsky@gmail.com>
2021-05-13 17:39:25 -0300

    elimino redundancia en atributos de la cuenta

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class Cuenta {
 
-  private double saldo = 0;
+  private double saldo;
   private List<Movimiento> movimientos = new ArrayList<>();
 
   public Cuenta() {
